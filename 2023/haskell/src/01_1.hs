ec0bf17bde9ea6d1e6e5bcf65840caa4931f1737
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-09-07 01:56:40 +0300

    add 2023 01 haskell

---
+++ 2023/haskell/src/01_1.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Data.Char (isDigit)
+import Data.List (tails)
+import Data.Maybe (listToMaybe, mapMaybe)
+
+wordDigitMap :: [(String, Char)]
+wordDigitMap = [("one", '1'), ("two", '2'), ("three", '3'), ("four", '4'), ("five", '5'), ("six", '6'), ("seven", '7'), ("eight", '8'), ("nine", '9')]
+
+findDigit :: String -> Maybe Char
+findDigit [] = Nothing
+findDigit (c : cs)
+  | isDigit c = Just c
+  | otherwise = findDigit cs
+
+firstDigit :: String -> Maybe Char
+firstDigit = listToMaybe . mapMaybe findDigit . tails
+
+lastDigit :: String -> Maybe Char
+lastDigit = listToMaybe . mapMaybe findDigit . reverse . tails
+
+processLine :: String -> Int
+processLine line =
+  case (firstDigit line, lastDigit line) of
+    (Just f, Just l) -> read [f, l]
+    _ -> 0
+
+main :: IO ()
+main = do
+  input <- readFile "input/01.txt"
+  print $ sum $ map processLine $ lines input
