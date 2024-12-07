cc0ade6c41ac8c5bc1bda10d3424a48f0779cfb8
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-12-07 21:58:30 +0200

    improve 2024 07 c++

--- 2024/cpp/days/07/02.cpp
+++ 2024/cpp/days/07/02.cpp
@@ -4,12 +4,11 @@
 #include <cstdint>
 #include <cstdlib>
 #include <fstream>
-#include <functional>
 #include <print>
 #include <ranges>
 #include <sstream>
+#include <stack>
 #include <string_view>
-#include <sys/types.h>
 #include <utility>
 #include <vector>
 
@@ -19,46 +18,41 @@ constexpr auto to_int = [](const std::ranges::view auto& str) {
 	return n;
 };
 
-static auto parse(std::string_view buffer) //
-	-> std::vector<std::pair<int64_t, std::vector<int64_t>>> {
-	return buffer																																//
-				 | std::views::split('\n')																						//
-				 | std::views::filter([](const auto& line) { return !line.empty(); }) //
+constexpr auto is_empty = [](const std::ranges::view auto& str) { return !str.empty(); };
+
+static auto parse(std::string_view buffer) -> std::ranges::view auto {
+	return buffer													//
+				 | std::views::split('\n')			//
+				 | std::views::filter(is_empty) //
 				 | std::views::transform([](const auto& line) {
 						 auto parts = line | std::views::split(':');
 						 auto it = parts.begin();
-						 auto result = to_int(*it);
-						 auto operands =
-							 *(++it) | std::views::split(' ')																						//
-							 | std::views::filter([](const auto& operand) { return !operand.empty(); }) //
-							 | std::views::transform(to_int)																						//
-							 | std::ranges::to<std::vector<int64_t>>();
-						 return std::make_pair(result, operands);
-					 }) //
-				 | std::ranges::to<std::vector<std::pair<int64_t, std::vector<int64_t>>>>();
+						 auto res = to_int(*it);
+						 auto ops = *(++it) | std::views::split(' ') //
+												| std::views::filter(is_empty)	 //
+												| std::views::transform(to_int)	 //
+												| std::ranges::to<std::vector<int64_t>>();
+						 return std::make_pair(res, ops);
+					 });
 }
-
-const auto operations = std::array<std::function<int64_t(int64_t, int64_t)>, 3>({
-	std::plus<>(),
-	std::multiplies<>(),
-	[](int64_t a, int64_t b) {
-		return (a * static_cast<int64_t>(std::pow(10, static_cast<int64_t>(std::log10(b) + 1)))) + b;
-	},
-});
 
 auto main(int /*argc*/, char** argv) -> int {
 	auto buffer = (std::ostringstream() << std::ifstream(argv[1]).rdbuf()).str();
 
-	auto equations = parse(buffer);
-	auto sum = std::ranges::fold_left(equations, 0ul, [](const auto& acc, const auto& eq) {
-		const auto& [result, operands] = eq;
-		for (auto i : std::views::iota(0, std::pow(operations.size(), operands.size() - 1))) {
-			auto res = std::ranges::fold_left_first(operands, [&i](const auto& a, const auto& b) {
-				auto res = operations[i % operations.size()](a, b);
-				i /= operations.size();
-				return res;
-			});
-			if (res == result) return acc + *res;
+	auto sum = std::ranges::fold_left(parse(buffer), 0ul, [](const auto& acc, const auto& eq) {
+		const auto& [res, ops] = eq;
+		auto stack = std::stack<std::pair<int64_t, size_t>>();
+		stack.emplace(res, ops.size() - 1);
+		while (!stack.empty()) {
+			const auto [target, i] = stack.top();
+			stack.pop();
+			if (target == 0) return acc + res;
+			if (i > ops.size() - 1) continue;
+			const auto op = ops[i];
+			const auto pot = static_cast<int64_t>(std::pow(10, std::floor(std::log10(op) + 1)));
+			if (target % op == 0) stack.emplace(target / op, i - 1);
+			if (target >= op) stack.emplace(target - op, i - 1);
+			if (target >= op && ((target - op) % pot) == 0) stack.emplace(target / pot, i - 1);
 		}
 		return acc;
 	});
