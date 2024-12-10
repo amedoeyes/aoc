459cf2981b0980284c2fc6a9e1bfad3c8f989278
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-12-10 17:50:56 +0200

    improve 2024 10 c++

--- 2024/cpp/days/10/02.cpp
+++ 2024/cpp/days/10/02.cpp
@@ -5,12 +5,8 @@
 #include <cstdlib>
 #include <fstream>
 #include <print>
-#include <ranges>
 #include <sstream>
 #include <stack>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
 
 struct hash {
 	auto operator()(const std::pair<size_t, size_t>& x) const -> size_t {
@@ -18,57 +14,31 @@ struct hash {
 	}
 };
 
-static auto parse(auto buffer) {
-	const auto width = buffer.find('\n');
-	const auto height = buffer.size() / (width + 1);
-	const auto map = buffer																									 //
-									 | std::views::filter([](char c) { return c != '\n'; })	 //
-									 | std::views::transform([](char c) { return c - '0'; }) //
-									 | std::ranges::to<std::vector<int64_t>>();
-	return std::make_tuple(map, width, height);
-}
+constexpr auto directions =
+	std::array<std::pair<int64_t, int64_t>, 4>({{0, 1}, {1, 0}, {0, -1}, {-1, 0}});
 
 auto main(int /*argc*/, char** argv) -> int {
 	const auto buffer = (std::ostringstream() << std::ifstream(argv[1]).rdbuf()).str();
-
-	const auto [map, w, h] = parse(buffer);
-
-	auto graph = std::unordered_map<
-		std::pair<size_t, size_t>,
-		std::unordered_set<std::pair<size_t, size_t>, hash>,
-		hash>();
-
-	for (const auto y : std::views::iota(0ul, h)) {
-		for (const auto x : std::views::iota(0ul, w)) {
-			for (const auto dy : std::views::iota(-1l, 2l)) {
-				for (const auto dx : std::views::iota(-1l, 2l)) {
-					if ((dx == 0 && dy == 0) || (dx != 0 && dy != 0) || dx + x >= w || dy + y >= h) continue;
-					graph[{x, y}].emplace(x + dx, y + dy);
-				}
-			}
-		}
-	}
-
-	auto trailheads = graph //
-										| std::views::transform([](const auto& pair) { return pair.first; })
-										| std::views::filter([&map, &w](const auto& pair) {
-												return map[(pair.second * w) + pair.first] == 0;
-											});
+	const auto w = buffer.find('\n') + 1;
+	const auto h = buffer.size() / w;
 
 	auto count = 0ul;
-	for (const auto& pair : trailheads) {
-		auto stack = std::stack<std::pair<size_t, size_t>>();
-		stack.emplace(pair);
-		while (!stack.empty()) {
-			const auto [x, y] = stack.top();
-			stack.pop();
-			if (map[(y * w) + x] == 9) {
-				++count;
-				continue;
-			}
-			for (const auto& [nx, ny] : graph[{x, y}]) {
-				if (static_cast<int64_t>(map[(ny * w) + nx]) - static_cast<int64_t>(map[(y * w) + x]) == 1)
-					stack.emplace(nx, ny);
+	auto stack = std::stack<std::pair<size_t, size_t>>();
+	for (auto y = 0ul; y < h; ++y) {
+		for (auto x = 0ul; x < w; ++x) {
+			if (buffer[(y * w) + x] != '0') continue;
+			stack.emplace(x, y);
+			while (!stack.empty()) {
+				const auto [x, y] = stack.top();
+				stack.pop();
+				for (const auto& [dx, dy] : directions) {
+					const auto nx = x + dx;
+					const auto ny = y + dy;
+					if (nx < w && ny < h && buffer[(ny * w) + nx] - buffer[(y * w) + x] == 1) {
+						if (buffer[(ny * w) + nx] == '9') ++count;
+						else stack.emplace(nx, ny);
+					}
+				}
 			}
 		}
 	}
